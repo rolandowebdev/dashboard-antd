3d4f4e3501b13ed415f4ef61ec5846951b0e748c
RolandoPranataAR <rolandopranata@gmail.com>
2023-06-07 15:57:22 +0700

    chore: define process.env into vite config

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,22 @@ export default ({ mode }) => {
 				},
 			}),
 		],
+		define: {
+			'process.env': {
+				VITE_SITE_URL: JSON.stringify(env.VITE_SITE_URL),
+				VITE_FIREBASE_API_KEY: JSON.stringify(env.VITE_FIREBASE_API_KEY),
+				VITE_FIREBASE_AUTH_DOMAIN: JSON.stringify(
+					env.VITE_FIREBASE_AUTH_DOMAIN
+				),
+				VITE_FIREBASE_PROJECT_ID: JSON.stringify(env.VITE_FIREBASE_PROJECT_ID),
+				VITE_FIREBASE_STORAGE_BUCKET: JSON.stringify(
+					env.VITE_FIREBASE_STORAGE_BUCKET
+				),
+				VITE_FIREBASE_MESSAGING_SENDER_ID: JSON.stringify(
+					env.VITE_FIREBASE_MESSAGING_SENDER_ID
+				),
+				VITE_FIREBASE_APP_ID: JSON.stringify(env.VITE_FIREBASE_APP_ID),
+			},
+		},
 	})
 }
